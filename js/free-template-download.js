2e17ea95d6de0184ae3562b97f7c2b7712b7b515
민지 <109532863+okdoll@users.noreply.github.com>
2023-08-17 13:16:00 +0900

    Update free-template-download.js

--- js/free-template-download.js
+++ js/free-template-download.js
@@ -3,3 +3,15 @@ const backButton = document.getElementById("back_btn");
 backButton.addEventListener("click", function() {
     window.location.href = "information-sharing.html"; // 이동할 페이지의 경로를 입력하세요
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const diaryBtn = document.querySelector(".diary_btn");
+    diaryBtn.addEventListener("click", function() {
+        window.location.href = "https://www.notion.so/ko-kr/personal";
+    });
+
+    const noteBtn = document.querySelector(".note_btn");
+    noteBtn.addEventListener("click", function() {
+        window.location.href = "https://www.goodnotes.com/kr"; // 여기에 원하는 다른 링크 주소를 넣으세요.
+    });
+});
